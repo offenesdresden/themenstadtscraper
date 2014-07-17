880d6971d637631ba9223da7f4ce6aa0beafb38e
Thomas Skowron <th@skowron.biz>
2014-07-17 15:44:17 +0200

    Queryerzeugung

--- themenstadtscraper.py
+++ themenstadtscraper.py
@@ -37,20 +37,36 @@ def handle_json_response(response_data, postgres_conn_string):
 	cur.close()
 
 
-def scrape(parallelity=4, postgres_conn_string="dbname=themenstadtplan"):
-	XMIN = 5400499
-	XMAX = 5427620
-	YMIN = 5649580
-	YMAX = 5672231
+def build_queries(xmin, xmax, ymin, ymax, basereq, granularity=10.):
+	g = 1/granularity
+	xstart = int(xmin*g)
+	xend = int((xmax+1)*g)
+	ystart = int(ymin*g)
+	yend = int((ymax+1)*g)
 
+	for x_g in range(xstart, xend):
+		for y_g in range(ystart, yend):
+			x = x_g*granularity
+			y = y_g*granularity
+			yield(basereq % (x, y))
+
+
+def scrape(parallelity=4, postgres_conn_string="dbname=themenstadtplan"):
+	XMIN = 5400499.
+	XMAX = 5427620.
+	YMIN = 5649580.
+	YMAX = 5672231.
 	BASEURL = "http://stadtplan2.dresden.de/(S(jcksai1ssxvzjbveqyv1aizy))/ajaxpro/ASP.spdd_controls_mapcontainer_ascx,App_Web_o3sebx4k.ashx"
 	HEADERS = {"X-AjaxPro-Method": "GetMapTipEx", "Origin": "http://stadtplan2.dresden.de", "Accept-Encoding": "gzip,deflate,sdch", "Accept-Language": "de-DE", "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36", "Content-Type": "text/plain; charset=UTF-8", "Accept": "*/*", "Referer": "http://stadtplan2.dresden.de/(S(jcksai1ssxvzjbveqyv1aizy))/spdd.aspx", "Cookie": "cardo3SessionGuid=C3_4ef68a3a-59ca-4a0c-b3cc-722fdabb1c84", "Connection": "keep-alive"}
+	BASEREQ = '{"applicationContextType":null,"themeList":"$BaseMap#5#Tiles","gx":%.6f,"gy":%.6f,"srs":31469,"currentMapScale":1500}'
 
-	with Pool(parallelity) as pool:
-		jsonreq = '{"applicationContextType":null,"themeList":"$BaseMap#5#Tiles","gx":%.6f,"gy":%.6f,"srs":31469,"currentMapScale":128000}' % (XMIN, YMIN)
+	for query in build_queries(XMIN, XMAX, YMIN, YMAX, basereq=BASEREQ, granularity=10.):
+		pass
 
-		res = requests.post(BASEURL, data=jsonreq, headers=HEADERS)
-		handle_json_response(res.json, postgres_getconn())
+	#with Pool(parallelity) as pool:
+
+	#res = requests.post(BASEURL, data=jsonreq, headers=HEADERS)
+	#handle_json_response(res.json(), postgres_conn_string)
 
 
 if __name__ == "__main__":
