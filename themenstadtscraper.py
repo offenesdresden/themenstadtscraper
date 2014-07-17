2e149b699228da9cc5123b4e66654549985b344e
Thomas Skowron <th@skowron.biz>
2014-07-17 17:10:49 +0200

    Läuft.

--- themenstadtscraper.py
+++ themenstadtscraper.py
@@ -29,6 +29,8 @@ def handle_json_response(response_data, postgres_cursor):
 	lat = response_data["value"]["PointInWgs"]["Y"]
 	lon = response_data["value"]["PointInWgs"]["X"]
 
+	print(lat, lon)
+
 	for record in response_data["value"]["Records"]:
 		write_to_db(postgres_cursor, record, lat, lon)
 
@@ -52,15 +54,22 @@ def scrape(parallelity=4, postgres_conn_string="dbname=themenstadtplan"):
 	XMAX = 5427620.
 	YMIN = 5649580.
 	YMAX = 5672231.
+
+	XMIN = 5412133.
+	XMAX = 5415148.
+	YMIN = 5656993.
+	YMAX = 5657370.
+
+
 	BASEURL = "http://stadtplan2.dresden.de/(S(jcksai1ssxvzjbveqyv1aizy))/ajaxpro/ASP.spdd_controls_mapcontainer_ascx,App_Web_o3sebx4k.ashx"
 	HEADERS = {"X-AjaxPro-Method": "GetMapTipEx", "Origin": "http://stadtplan2.dresden.de", "Accept-Encoding": "gzip,deflate,sdch", "Accept-Language": "de-DE", "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36", "Content-Type": "text/plain; charset=UTF-8", "Accept": "*/*", "Referer": "http://stadtplan2.dresden.de/(S(jcksai1ssxvzjbveqyv1aizy))/spdd.aspx", "Cookie": "cardo3SessionGuid=C3_4ef68a3a-59ca-4a0c-b3cc-722fdabb1c84", "Connection": "keep-alive"}
-	BASEREQ = '{"applicationContextType":null,"themeList":"$BaseMap#5#Tiles","gx":%.6f,"gy":%.6f,"srs":31469,"currentMapScale":1500}'
+	BASEREQ = '{"applicationContextType":null,"themeList":"$BaseMap#5#Tiles","gx":%.6f,"gy":%.6f,"srs":31469,"currentMapScale":1000}'
 
 	with psycopg2.connect(postgres_conn_string) as pg_conn:
 		pg_conn.autocommit = True
 		with pg_conn.cursor() as pg_cursor:
 			for query in build_queries(XMIN, XMAX, YMIN, YMAX, basereq=BASEREQ, granularity=10.):
-				res = requests.post(BASEURL, data=jsonreq, headers=HEADERS)
+				res = requests.post(BASEURL, data=query, headers=HEADERS)
 				handle_json_response(res.json(), pg_cursor)
 
 
